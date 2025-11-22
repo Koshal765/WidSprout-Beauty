e749882fcdb98ea4ed4fb63146bac4c958510763
Koshal765 <koshalpothare@gamil.com>
2025-11-22 22:38:48 +0530

    added image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter';
+import banner from './assets/banner.png'
 
 const Home = () => {
   return (
-    <div className='text-center mt-40'>
-        <div className='text-xl md:text-2xl lg:text-4xl'>
+    <div className='text-center mt-10'>
+      <div
+    className="flex  justify-center h-[200px] md:h-[410px] lg:h-[500px] w-full bg-center bg-contain bg-no-repeat rounded-lg  "
+    style={{
+      backgroundImage: `url(${banner})`,
+    }}
+  >
+    <h1 className='flex items-end text-md md:font-3xl lg:font-2xl font-semibold text-gray-700 mb-10'>
         <Typewriter
             words={['Welcome to WildSprout Beauty', 'Launching Soon...']}
             loop={0}    
@@ -13,7 +20,7 @@ const Home = () => {
             typeSpeed={70}
             deleteSpeed={50}
             delaySpeed={1000}
-        /></div>
+        /></h1></div>
        
     </div>
   )
