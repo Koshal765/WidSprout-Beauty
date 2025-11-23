2358392c39dc79ca70cb0a7665e9353fee9f473e
Koshal765 <koshalpothare@gamil.com>
2025-11-23 20:11:29 +0530

    made some changes with submit button

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -10,6 +10,8 @@ const Feedback = ({setRefresh}) => {
     message:""
   });
 
+  const[btndisabled,setBtndisabled]=useState(false);
+
   const reset = () => {
     setIsfilled({
       name:"",
@@ -21,6 +23,8 @@ const Feedback = ({setRefresh}) => {
   const handelSubmit = async(e) => {
    e.preventDefault();
 
+   setBtndisabled(true);
+
    try{
       const res = await axios.post("https://6922dcec09df4a4923238a99.mockapi.io/Product_reviewa",isfilled);
   console.log(res.data);
@@ -28,6 +32,7 @@ const Feedback = ({setRefresh}) => {
     reset();
 
   setRefresh(prev => prev + 1);
+  setBtndisabled(false);
 
    }catch(error){
     toast.error("Error in submitting feedback. Please try again.",error)
@@ -45,8 +50,10 @@ const Feedback = ({setRefresh}) => {
   } 
 
   return (
-   <section id='Feedback' className='my-20'>
-   <div className=' justify-center flex'>
+   <section id='Feedback' className='my-20 py-24'>
+    <h1 className='text-center text-xl md:text-2xl lg:text-5xl font-serif font-semibold'>Share your Feedback </h1>
+    <p className='text-center font-serif text-2xl mt-10 text-gray-700'>We value your experience! Your feedback helps us improve and serve you better.</p>
+   <div className=' justify-center flex mt-19'>
     <form onSubmit={handelSubmit}> 
        <div className='text-center  w-75  md:w-120 lg:w-120 rounded-xl shadow-lg shadow-gray-400 border border-amber-100'>
       <h1 className='text-center mt-5 font-serif text-lg md:text-2xl lg:text-3xl'>Feedback Form</h1>
@@ -58,7 +65,7 @@ const Feedback = ({setRefresh}) => {
         
         <textarea name="message" placeholder='Enter your feedback ' value={isfilled.message} onChange={handelChange} className='m-5 p-2 rounded-lg w-50 md:w-96 h-32 border border-orange-300' rows={2} required/>
       <br/>
-        <button type="submit"  className='mb-5 p-2 bg-yellow-400 rounded-xl hover:bg-yellow-500 transition-all hover:scale-105 w-50 md:w-100 lg:w-100'>Submit</button>
+        <button type="submit" disabled={btndisabled} className='mb-5 p-2 bg-yellow-400 rounded-xl hover:bg-yellow-500 transition-all hover:scale-105 w-50 md:w-100 lg:w-100'>{btndisabled ? "Submitting..." : "Submit"}</button>
       </div>
     </form>
      <ToastContainer autoClose={2000} theme='dark' transition={Slide} closeOnClick:true   draggable:true hideProgressBar={false}/>
--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -34,11 +34,11 @@ const Reviews = ({refresh}) => {
   return (
     <section id='Reviews' className='py-24'>
       <div className='text-center'>
-        <h1 className='text-3xl font-serif mb-10'>⭐Customer Reviews</h1>
+        <h1 className=' text-3xl md:text-4xl lg:text-5xl font-serif mb-10'>⭐Customer Reviews</h1>
         <p className='text-lg w-75 md:w-100 lg:w-full mx-auto'>"Here’s what our customers have to say about their experience with us."</p>
         <p className='mt-5 font-semibold'>We value our customers, and their feedback helps us grow.</p>
       </div>
-      <div className='flex justify-center flex-wrap gap-5'>
+      <div className='flex justify-center flex-wrap gap-5 w-full'>
       {loading ? (<div><h1>loading Customers Reviews</h1></div> ) : (  reviews.length === 0 ? (<div className='w-full  flex justify-center'>
         <div className='w-75 h-auto border border-amber-300 rounded-lg p-5 m-5 shadow-lg shadow-gray-400'>
         <h1 className='text-center  '>No Reviews Available</h1>
@@ -47,10 +47,10 @@ const Reviews = ({refresh}) => {
       <div className='w-full flex flex-wrap justify-center gap-5 mt-5'>
     
        {reviews.map((rev)=>(
-        <div key={rev.id} className=' w-75 h-auto border border-amber-300 rounded-lg p-5 m-5 shadow-lg shadow-gray-400 transition-all hover:scale-105 flex flex-col justify-between'>
+        <div key={rev.id} className=' w-70 md:w-100 lg:w-100 h-auto border border-amber-300 rounded-lg p-5 m-5 shadow-lg shadow-gray-400 transition-all hover:scale-105 flex flex-col justify-between'>
             <div className='grow'>
-            <p className='text-2xl font-medium'>{rev.message}</p>
-            <p className='text-xl mt-3'>{rev.rating}⭐</p> 
+            <p className='text-xl  md:text-2xl lg:text-2xl font-medium'>{rev.message}</p>
+            <p className='text-xl md:text-2xl lg:text-2xl mt-3'>{rev.rating}⭐</p> 
           </div> 
             <h1 className='mt-2 font-semibold '>-{rev.name}</h1>
             
